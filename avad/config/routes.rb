91574c8a6bdc35834ce2059623bc1346c7588d94
Sean Ryan Chan <seanryachan@gmail.com>
2017-02-18 12:12:04 +0800

    routed to splash

--- avad/config/routes.rb
+++ avad/config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :events
-  
+  # Landing Page
+  root 'splash#index'
+
+
 end
