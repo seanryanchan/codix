3e7de2cb6014368c30ca74b22677339f9ba9bba0
gablooblue <gabrielhenry.lopez@gmail.com>
2017-02-18 15:32:57 +0800

    Routing edited

--- avad/config/routes.rb
+++ avad/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   get 'splash/index', as: 'splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :events
