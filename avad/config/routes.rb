c6e01635f3e0309ab13022342f0705d710b2f561
gablooblue <gabrielhenry.lopez@gmail.com>
2017-02-18 12:31:20 +0800

    Added toolbar and some routing

--- avad/config/routes.rb
+++ avad/config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'splash/index'
-
+  get 'splash/index', as: 'splash'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :events
   
+   # Landing Page
+  root 'splash#index'
+
+  #TODO Users routing
 end
